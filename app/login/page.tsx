86a5bc32c2fa087f6bf266b8c750ad85dbda82b6
wisemantjieva-a11y <wisemantjieva@gmail.com>
2026-07-03 23:21:34 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,8 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import { apiFetch } from '@/lib/api'
 
+export const dynamic = 'force-dynamic'
+
 export default function Login() {
   const router = useRouter()
   const searchParams = useSearchParams()
